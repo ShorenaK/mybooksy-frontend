e411c4d228b4f0038b9433c03a8fa6f3e09906b2
ShorenaK <shorenaanzhilov@gmail.com>
2022-09-19 16:35:21 -0400

    App compo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,16 @@
-import { Routes, Route, Outlet } from "react-router-dom"
+import { Routes, Route, Outlet } from "react-router-dom";
+import { Books } from "../pages/Books";
+import Show from "../pages/Show";
 
 function Main(props) {
-  const URL = "https://mybooksy-project.herokuapp.com/"
+ 
  
     return ( 
        <>
        <Outlet/>
        <Routes>
-      
+       <Route exact path="/books" element={<Books />} />  
+        <Route  exact path="/books/:bookId" element={<Show />}/> 
       </Routes>
       <Footer />
      </> 
