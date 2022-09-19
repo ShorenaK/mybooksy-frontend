6748bfb1b19bab8c04c485b785db9154be1d682b
ShorenaK <shorenaanzhilov@gmail.com>
2022-09-19 16:48:29 -0400

    Added components & refactor

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,4 +1,7 @@
+// This page will fatch the Books data 
 
+// import { useState, useEffect } from "react";
+// import {Link} from 'react-router-dom' 
 
 export const Books = () => {
     const URL = "https://mybooksy-project.herokuapp.com/books"
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// delete and update page 
 
 export const Show = () => {
   return (
