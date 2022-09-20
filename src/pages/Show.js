3040d77f690c84d687b33ef473e6f0122ea3eb95
ShorenaK <shorenaanzhilov@gmail.com>
2022-09-19 21:45:23 -0400

    show page setup

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,24 @@
 // Delete and update page 
-const { id } = useParams()
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 export const Show = () => {
+  const [book, setBook ]= useState(null)
+  const { id } = useParams()
+  const URL = `http://localhost:4000/books/${id}`
+     
+  const getBook = async ()=>{
+    try {
+      const response = await fetch(URL)
+      const result = await response.json()
+      setBook(result)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+console.log(`current book ${JSON.stringify(book)}`)
+
   return (
     <div>Show</div>
   )
