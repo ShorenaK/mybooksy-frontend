ec3316a4e415233cc017ed4dc827c9e5406be7e4
Jordan-Burgess <alfredjordan93@gmail.com>
2022-09-20 18:59:09 -0400

    Merge branch with Development

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,8 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import { Books } from "../pages/Books";
 import {Show} from "../pages/Show";
 import BookForm  from "./BookForm"
-<<<<<<< HEAD
 import ReviewForm from "./ReviewForm"
-=======
 import EditForm from "./EditForm";
->>>>>>> c9e39d3c20981cedd3818df968db47296b0be9bb
 
 
 
@@ -19,11 +16,8 @@ function Main(props) {
        <Route exact path="/books" element={<Books />} />  
         <Route  exact path="/books/:bookId" element={<Show />}/> 
         <Route exact path="/books/add" element={<BookForm />}/>
-<<<<<<< HEAD
         <Route exact path="/reviews/:bookId/add" element={<ReviewForm />} />
-=======
         <Route exact path="/books/:bookId/edit" element={<EditForm />}/>
->>>>>>> c9e39d3c20981cedd3818df968db47296b0be9bb
       </Routes>
      </> 
      ) 
