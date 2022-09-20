730706522954af2dae465f107ec9237111068ee3
ShorenaK <shorenaanzhilov@gmail.com>
2022-09-19 20:26:10 -0400

    data feetched & BrowserRouter added

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,11 @@
 // This page will fatch the Books data 
 
  import { useState, useEffect } from "react";
-// import {Link} from 'react-router-dom' 
+//  import {Link} from 'react-router-dom' 
 
 export const Books = (props) => {
     const [books, setBooks] = useState([])
-    const URL = "https://mybooksy-project.herokuapp.com/books"
+    const URL = "https://mybooksy-project.herokuapp.com/books"  
     const getBooks = async () =>{
         try{
           const response = await fetch(URL)
@@ -15,21 +15,22 @@ export const Books = (props) => {
           console.log(err)
         }
     }
- useEffect(()=>{getBooks}, [])
+ useEffect(() => { 
+  getBooks()
+ }, [])
 
 const loaded = ()=>{
   return books?.map((book)=>{
        return (
         <div key={book._id}>
           <h2>{book.title}</h2>
-          <img src={book.image}  />
+          <img src={book.image}  alt="book"/>
            <h3>{book.author}</h3>
            <h4>{book.genre}</h4>
            <p>{book.pages}</p>
            <p>{book.description}</p>
            <p>{book.publishDate}</p>
-           <p>{book.link}</p>
-        
+            <p>{book.link}</p>
            <p>{book.likes}</p>
            <p>{book.__v}</p>
         </div>
@@ -44,6 +45,7 @@ const loading = () => (
         <img
           className="spinner"
           src="https://freesvg.org/img/1544764567.png"
+          alt="sipnner"
         />{" "}
       </span>
     </h1>
@@ -51,7 +53,7 @@ const loading = () => (
 );
   return (
     <div className="book-list"> {books ? loaded() : loading() }
-    
+
     </div>
   )
 }
